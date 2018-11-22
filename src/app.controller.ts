b60db51137d3dfffdc184d9426b86ecd7466bbaf
wsq <mywsq961@gmail.com>
2018-11-22 18:43:55 +0800

    新增测试API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Auth } from 'auth/auth.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
-  }
+	@Get()
+	@Auth()
+	hello() {
+		return 'Hello Wrold!';
+	}
 }
